bcaee84b285f038fc37b885188e5a96892a65b43
Gilang Adi <novalinobramasta1@gmail.com>
2019-04-20 12:10:56 +0700

    menambahkan file bernama proyek-laravel

---
+++ pryek-laravel/app/Http/Controllers/ControllerBersama.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerBersama extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+
+        $nama = "Gilang Adi Saputra";
+        $NIM  = 361755401189;
+
+          return view('tampilan',compact('nama','NIM'));
+    }
+
+    public function aku(){
+
+        $kewan = ['coro','nyambek','ulo'];
+          return view('tampilan1') -> with('kewan', $kewan);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ pryek-laravel/resources/views/tampilan.blade.php
@@ -0,0 +1,5 @@
+@section('konten')
+<h2> Nama   : {{$nama}} </h2>
+<h2> NIM    : {{$NIM}} </h2>
+<br>
+<h2>ternyata namanya {{$nama}} NIM nya {{$NIM}}</h2>
