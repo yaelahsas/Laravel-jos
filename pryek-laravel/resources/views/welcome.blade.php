400566516ccc9549af14060a39e60dd6dd373a83
Gilang292 <49708054+Gilang292@users.noreply.github.com>
2019-04-20 23:42:25 +0700

    menambahkan lalimat pada welcome di resources

--- pryek-laravel/resources/views/welcome.blade.php
+++ pryek-laravel/resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-                Gilang Adi Saputra
+                Gilang Adi Saputra1
             </div>
         </div>
     </body>
